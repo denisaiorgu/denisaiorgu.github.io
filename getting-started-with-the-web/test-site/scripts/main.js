c1d41e7b4f7b3483fb487bbdb6c8d76fe094682f
Denisa Iorgu <166735438+denisaiorgu@users.noreply.github.com>
2024-06-13 22:54:53 +0300

    added the getting-started-with-the-web/test-site files

---
+++ getting-started-with-the-web/test-site/scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/download.jpg") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/download.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `DogGo is cool, ${myName}`;
+    }
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `DogGo is cool, ${storedName}`;
+  }
+  
+  myButton.onclick = () => {
+    setUserName();
+  };
